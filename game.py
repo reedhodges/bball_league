317c7d8c94514a307202a8782ed91a8dd9a60cdc
Reed Hodges <61560859+reedhodges@users.noreply.github.com>
2023-10-31 16:26:26 -0400

    made more general classes

--- game.py
+++ game.py
@@ -3,76 +3,54 @@ from rosters import truncated_norm, team
 
 class game:
     def __init__(self, team1, team2):
+        np.random.seed(None)
+        
         self.team1 = team1
         self.team2 = team2
+        self.positions = ['pg', 'sg', 'sf', 'pf', 'c']
 
-        # matchup penalties: if a player's overall rating is lower than the opposing player's,
-        # reduce their expected stats by that percentage
-        self.pg_penalty = (team1.pg.overall - team2.pg.overall) / 100.
-        self.sg_penalty = (team1.sg.overall - team2.sg.overall) / 100.
-        self.sf_penalty = (team1.sf.overall - team2.sf.overall) / 100.
-        self.pf_penalty = (team1.pf.overall - team2.pf.overall) / 100.
-        self.c_penalty = (team1.c.overall - team2.c.overall) / 100.
-
-        self.team1_pg = np.ceil(np.array([truncated_norm(team1.pg.pts * (1+self.pg_penalty) , 5), 
-                          truncated_norm(team1.pg.reb * (1+self.pg_penalty) , 5),
-                          truncated_norm(team1.pg.ast * (1+self.pg_penalty) , 5)]))
-        self.team1_sg = np.ceil(np.array([truncated_norm(team1.sg.pts * (1+self.sg_penalty) , 5),
-                            truncated_norm(team1.sg.reb * (1+self.sg_penalty) , 5),
-                            truncated_norm(team1.sg.ast * (1+self.sg_penalty) , 5)]))
-        self.team1_sf = np.ceil(np.array([truncated_norm(team1.sf.pts * (1+self.sf_penalty) , 5),
-                            truncated_norm(team1.sf.reb * (1+self.sf_penalty) , 5),
-                            truncated_norm(team1.sf.ast * (1+self.sf_penalty) , 5)]))
-        self.team1_pf = np.ceil(np.array([truncated_norm(team1.pf.pts * (1+self.pf_penalty) , 5),
-                            truncated_norm(team1.pf.reb * (1+self.pf_penalty) , 5),
-                            truncated_norm(team1.pf.ast * (1+self.pf_penalty) , 5)]))
-        self.team1_c = np.ceil(np.array([truncated_norm(team1.c.pts * (1+self.c_penalty) , 5),
-                            truncated_norm(team1.c.reb * (1+self.c_penalty) , 5),
-                            truncated_norm(team1.c.ast * (1+self.c_penalty) , 5)]))
+        # Calculate penalties for each position
+        self.penalties = {pos: (getattr(team1, pos).overall - getattr(team2, pos).overall) / 100. 
+                          for pos in self.positions}
         
-        self.team2_pg = np.ceil(np.array([truncated_norm(team2.pg.pts * (1-self.pg_penalty) , 5),
-                            truncated_norm(team2.pg.reb * (1-self.pg_penalty) , 5),
-                            truncated_norm(team2.pg.ast * (1-self.pg_penalty) , 5)]))
-        self.team2_sg = np.ceil(np.array([truncated_norm(team2.sg.pts * (1-self.sg_penalty) , 5),
-                            truncated_norm(team2.sg.reb * (1-self.sg_penalty) , 5),
-                            truncated_norm(team2.sg.ast * (1-self.sg_penalty) , 5)]))
-        self.team2_sf = np.ceil(np.array([truncated_norm(team2.sf.pts * (1-self.sf_penalty) , 5),
-                            truncated_norm(team2.sf.reb * (1-self.sf_penalty) , 5),
-                            truncated_norm(team2.sf.ast * (1-self.sf_penalty) , 5)]))
-        self.team2_pf = np.ceil(np.array([truncated_norm(team2.pf.pts * (1-self.pf_penalty) , 5),
-                            truncated_norm(team2.pf.reb * (1-self.pf_penalty) , 5),
-                            truncated_norm(team2.pf.ast * (1-self.pf_penalty) , 5)]))
-        self.team2_c = np.ceil(np.array([truncated_norm(team2.c.pts * (1-self.c_penalty) , 5),
-                            truncated_norm(team2.c.reb * (1-self.c_penalty) , 5),
-                            truncated_norm(team2.c.ast * (1-self.c_penalty) , 5)]))
+        # Calculate box scores for both teams
+        self.team1_box = self.calculate_box_score(team1, 1)
+        self.team2_box = self.calculate_box_score(team2, -1)
 
-        self.team1_box = np.vstack((self.team1_pg, self.team1_sg, self.team1_sf, self.team1_pf, self.team1_c))
-        self.team2_box = np.vstack((self.team2_pg, self.team2_sg, self.team2_sf, self.team2_pf, self.team2_c))
-        
+    def calculate_box_score(self, team, penalty_multiplier):
+        box_scores = []
+        for pos in self.positions:
+            player = getattr(team, pos)
+            penalty = self.penalties[pos] * penalty_multiplier
+            pts = np.ceil(truncated_norm(player.pts * (1 + penalty), 5))
+            reb = np.ceil(truncated_norm(player.reb * (1 + penalty), 5))
+            ast = np.ceil(truncated_norm(player.ast * (1 + penalty), 5))
+            box_scores.append([pts, reb, ast])
+        return np.array(box_scores)
+
+    # Display box score for a team
+    def display_box(self, team_box, team):
+        print(team.name)
+        for i, pos in enumerate(self.positions):
+            print(f"{pos.upper()}: {team_box[i, 0]} pts, {team_box[i, 1]} reb, {team_box[i, 2]} ast")
+        print()
+
+    # Display box scores for both teams
     def box_score(self):
-        print('Team 1')
-        print('PG: {} pts, {} reb, {} ast'.format(self.team1_box[0,0], self.team1_box[0,1], self.team1_box[0,2]))
-        print('SG: {} pts, {} reb, {} ast'.format(self.team1_box[1,0], self.team1_box[1,1], self.team1_box[1,2]))
-        print('SF: {} pts, {} reb, {} ast'.format(self.team1_box[2,0], self.team1_box[2,1], self.team1_box[2,2]))
-        print('PF: {} pts, {} reb, {} ast'.format(self.team1_box[3,0], self.team1_box[3,1], self.team1_box[3,2]))
-        print('C: {} pts, {} reb, {} ast'.format(self.team1_box[4,0], self.team1_box[4,1], self.team1_box[4,2]))
+        self.display_box(self.team1_box, self.team1)
+        self.display_box(self.team2_box, self.team2)
 
-        print('Team 2')
-        print('PG: {} pts, {} reb, {} ast'.format(self.team2_box[0,0], self.team2_box[0,1], self.team2_box[0,2]))
-        print('SG: {} pts, {} reb, {} ast'.format(self.team2_box[1,0], self.team2_box[1,1], self.team2_box[1,2]))
-        print('SF: {} pts, {} reb, {} ast'.format(self.team2_box[2,0], self.team2_box[2,1], self.team2_box[2,2]))
-        print('PF: {} pts, {} reb, {} ast'.format(self.team2_box[3,0], self.team2_box[3,1], self.team2_box[3,2]))
-        print('C: {} pts, {} reb, {} ast'.format(self.team2_box[4,0], self.team2_box[4,1], self.team2_box[4,2]))
-
+    # Display final score
     def result(self):
-        team1_pts = np.sum(self.team1_box[1:,0])
-        team2_pts = np.sum(self.team2_box[1:,0])
-        print('Team 1: {} points'.format(team1_pts))
-        print('Team 2: {} points'.format(team2_pts))
-        
-team1 = team()
-team2 = team()
+        team1_pts = np.sum(self.team1_box[:, 0])
+        team2_pts = np.sum(self.team2_box[:, 0])
+        print(f'{self.team1.name}: {team1_pts} points')
+        print(f'{self.team2.name}: {team2_pts} points')
 
-game1 = game(team1, team2)
+        
+eagles = team(name="Eagles",seed=1)
+falcons = team(name="Falcons",seed=2)
+
+game1 = game(eagles, falcons)
 game1.box_score()
 game1.result()
--- rosters.py
+++ rosters.py
@@ -4,120 +4,98 @@ from scipy.stats import truncnorm
 
 # use a truncated normal distribution to generate a random
 # skill rating between 0 and 100, for a given mean and std dev
-def truncated_norm(mean, std_dev):
+def truncated_norm(mean, std_dev, seed=None):
+    # make sure the seed is set so that the same player is generated
+    if seed is not None:
+        np.random.seed(seed)
     a = (0 - mean) / std_dev
     b = (100 - mean) / std_dev
     return truncnorm.rvs(a, b, loc=mean, scale=std_dev)
 
-class pg:
-    def __init__(self):
-        # attributes
-        self.outside_scoring = truncated_norm(80,15)
-        self.inside_scoring = truncated_norm(60,10)
-        self.defending = truncated_norm(70,20)
-        self.athleticism = truncated_norm(80,10)
-        self.playmaking = truncated_norm(85,10)
-        self.rebounding = truncated_norm(60,15)
-        self.intangibles = truncated_norm(70,5)
-        self.attributes = [self.outside_scoring, self.inside_scoring, self.defending, 
-                self.athleticism, self.playmaking, self.rebounding, self.intangibles]
-        # define overall to be the average of the top 3 attributes
-        self.overall = np.mean(sorted(self.attributes)[-3:])
-
-        # define expected values of a player's stats
-        self.pts = 0.1 * (self.outside_scoring + self.inside_scoring)
-        self.reb = 0.05 * self.rebounding
-        self.ast = 0.05 * self.playmaking
-
-class sg:
-    def __init__(self):
-        # attributes
-        self.outside_scoring = truncated_norm(85,15)
-        self.inside_scoring = truncated_norm(60,10)
-        self.defending = truncated_norm(70,20)
-        self.athleticism = truncated_norm(80,10)
-        self.playmaking = truncated_norm(75,15)
-        self.rebounding = truncated_norm(60,15)
-        self.intangibles = truncated_norm(70,5)
-        self.attributes = [self.outside_scoring, self.inside_scoring, self.defending, 
-                self.athleticism, self.playmaking, self.rebounding, self.intangibles]
-        # define overall to be the average of the top 3 attributes
-        self.overall = np.mean(sorted(self.attributes)[-3:])
-
-        # define expected values of a player's stats
-        self.pts = 0.1 * (self.outside_scoring + self.inside_scoring)
-        self.reb = 0.05 * self.rebounding
-        self.ast = 0.05 * self.playmaking
-
-class sf:
-    def __init__(self):
-        # attributes
-        self.outside_scoring = truncated_norm(75,15)
-        self.inside_scoring = truncated_norm(75,15)
-        self.defending = truncated_norm(75,15)
-        self.athleticism = truncated_norm(75,15)
-        self.playmaking = truncated_norm(60,15)
-        self.rebounding = truncated_norm(75,15)
-        self.intangibles = truncated_norm(70,5)
-        self.attributes = [self.outside_scoring, self.inside_scoring, self.defending, 
-                self.athleticism, self.playmaking, self.rebounding, self.intangibles]
-        # define overall to be the average of the top 3 attributes
-        self.overall = np.mean(sorted(self.attributes)[-3:])
-
-        # define expected values of a player's stats
-        self.pts = 0.1 * (self.outside_scoring + self.inside_scoring)
-        self.reb = 0.05 * self.rebounding
-        self.ast = 0.05 * self.playmaking
-
-class pf:
-    def __init__(self):
-        # attributes
-        self.outside_scoring = truncated_norm(60,15)
-        self.inside_scoring = truncated_norm(80,10)
-        self.defending = truncated_norm(80,10)
-        self.athleticism = truncated_norm(70,15)
-        self.playmaking = truncated_norm(60,15)
-        self.rebounding = truncated_norm(80,15)
-        self.intangibles = truncated_norm(70,5)
-        self.attributes = [self.outside_scoring, self.inside_scoring, self.defending, 
-                self.athleticism, self.playmaking, self.rebounding, self.intangibles]
-        # define overall to be the average of the top 3 attributes
-        self.overall = np.mean(sorted(self.attributes)[-3:])
-
-        # define expected values of a player's stats
-        self.pts = 0.1 * (self.outside_scoring + self.inside_scoring)
-        self.reb = 0.05 * self.rebounding
-        self.ast = 0.05 * self.playmaking
-
-class c:
-    def __init__(self):
-        # attributes
-        self.outside_scoring = truncated_norm(50,15)
-        self.inside_scoring = truncated_norm(85,15)
-        self.defending = truncated_norm(80,15)
-        self.athleticism = truncated_norm(60,10)
-        self.playmaking = truncated_norm(60,15)
-        self.rebounding = truncated_norm(85,5)
-        self.intangibles = truncated_norm(70,5)
-        self.attributes = [self.outside_scoring, self.inside_scoring, self.defending, 
-                self.athleticism, self.playmaking, self.rebounding, self.intangibles]
-        # define overall to be the average of the top 3 attributes
-        self.overall = np.mean(sorted(self.attributes)[-3:])
-
-        # define expected values of a player's stats
-        self.pts = 0.1 * (self.outside_scoring + self.inside_scoring)
-        self.reb = 0.05 * self.rebounding
-        self.ast = 0.05 * self.playmaking
-
+class Player:
+    def __init__(self, attribute_means_and_stds, seed=None):
+        self.attributes = {}
+        for key, (mean, std) in attribute_means_and_stds.items():
+            self.attributes[key] = truncated_norm(mean, std, seed)
         
+        # Define overall to be the average of the top 3 attributes
+        self.overall = np.mean(sorted(self.attributes.values())[-3:])
+        
+        # Define expected values of a player's stats
+        self.pts = 0.1 * (self.attributes['outside_scoring'] + self.attributes['inside_scoring'])
+        self.reb = 0.05 * self.attributes['rebounding']
+        self.ast = 0.05 * self.attributes['playmaking']
+
+class pg(Player):
+    def __init__(self, seed=None):
+        super().__init__({
+            'outside_scoring': (80, 15),
+            'inside_scoring': (60, 10),
+            'defending': (70, 20),
+            'athleticism': (80, 10),
+            'playmaking': (85, 10),
+            'rebounding': (60, 15),
+            'intangibles': (70, 5)
+        }, seed=seed)
+
+class sg(Player):
+    def __init__(self, seed=None):
+        super().__init__({
+            'outside_scoring': (85, 15),
+            'inside_scoring': (60, 10),
+            'defending': (70, 20),
+            'athleticism': (80, 10),
+            'playmaking': (75, 15),
+            'rebounding': (60, 15),
+            'intangibles': (70, 5)
+        }, seed=seed)
+
+class sf(Player):
+    def __init__(self, seed=None):
+        super().__init__({
+            'outside_scoring': (75, 15),
+            'inside_scoring': (75, 15),
+            'defending': (75, 10),
+            'athleticism': (60, 15),
+            'playmaking': (60, 15),
+            'rebounding': (75, 15),
+            'intangibles': (70, 5)
+        }, seed=seed)
+
+class pf(Player):
+    def __init__(self, seed=None):
+        super().__init__({
+            'outside_scoring': (60, 15),
+            'inside_scoring': (80, 10),
+            'defending': (75, 10),
+            'athleticism': (70, 15),
+            'playmaking': (60, 15),
+            'rebounding': (80, 15),
+            'intangibles': (70, 5)
+        }, seed=seed)
+
+class c(Player):
+    def __init__(self, seed=None):
+        super().__init__({
+            'outside_scoring': (50, 15),
+            'inside_scoring': (85, 15),
+            'defending': (80, 10),
+            'athleticism': (60, 10),
+            'playmaking': (60, 15),
+            'rebounding': (85, 5),
+            'intangibles': (70, 5)
+        }, seed=seed)
+
 class team:
-    def __init__(self):
+    def __init__(self, name, seed=None):
+        self.name = name
+        self.seed = seed
         # roster
-        self.pg = pg()
-        self.sg = sg()
-        self.sf = sf()
-        self.pf = pf()
-        self.c = c()
+        self.pg = pg(seed=self.seed)
+        self.sg = sg(seed=self.seed)
+        self.sf = sf(seed=self.seed)
+        self.pf = pf(seed=self.seed)
+        self.c = c(seed=self.seed)
         # overall rating
         self.overall = np.mean([self.pg.overall, self.sg.overall, self.sf.overall, self.pf.overall, self.c.overall])
 
