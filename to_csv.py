23cab1af440a3447229ba75b4105d798f3271623
Reed Hodges <61560859+reedhodges@users.noreply.github.com>
2023-11-08 09:45:09 -0500

    added team stats csv generation

--- to_csv.py
+++ to_csv.py
@@ -1,16 +1,17 @@
 import pandas as pd
 from season import season
+from math import trunc
 
 s = season(30)
-data = s.stats
+game_data = s.stats
 
 # flatten array first
 entries = []
-for team in range(data.shape[0]):
-    for game in range(data.shape[1]):
-        for position in range(data.shape[2]):
-            for stat in range(data.shape[3]):
-                entries.append((team, game, position, stat, data[team, game, position, stat]))
+for team in range(game_data.shape[0]):
+    for game in range(game_data.shape[1]):
+        for position in range(game_data.shape[2]):
+            for stat in range(game_data.shape[3]):
+                entries.append((team, game, position, stat, game_data[team, game, position, stat]))
 
 # convert to dataframe
 df_stats = pd.DataFrame(entries, columns=['team', 'game_id', 'pos', 'stat', 'val'])
@@ -23,5 +24,13 @@ df_stats.pos = df_stats.pos.apply(lambda x: s.positions[x])
 df_stats.stat = df_stats.stat.apply(lambda x: s.stat_names[x])
 
 df_stats.to_csv('game_stats.csv', index=False)
+
+# now make a dataframe with for team stats
+# columns: team name, wins, losses, draws
+entries = []
+for team in range(s.num_teams):
+    entries.append((s.teams[team].name, trunc(s.team_stats[team, 0]), trunc(s.team_stats[team, 1]), trunc(s.team_stats[team, 2])))
+df_team_stats = pd.DataFrame(entries, columns=['team', 'W', 'L', 'D'])
+df_team_stats.to_csv('team_stats.csv', index=False)
 
 
